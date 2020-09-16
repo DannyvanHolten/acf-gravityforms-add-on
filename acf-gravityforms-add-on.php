5e477aa07d16a01ba4228ac30376ce24cf14e3e6
Mark Howells-Mead <permanent.tourist@gmail.com>
2020-09-16 18:37:50 +0100

    Version bump

--- acf-gravityforms-add-on.php
+++ acf-gravityforms-add-on.php
@@ -3,7 +3,7 @@
 Plugin Name: Advanced Custom Fields: Gravityforms Add-on
 Plugin URI: https://github.com/dannyvanholten/acf-gravityforms-add-on
 Description: Advanced Custom Field with which we can select Gravityforms.
-Version: 1.2.4
+Version: 1.2.5
 Author: Danny van Holten
 Author URI: http://www.dannyvanholten.com/
 Copyright: Danny van Holten
@@ -14,7 +14,7 @@ if (!defined('ABSPATH')) {
 } // Exit if accessed directly
 
 // Define multiple necessary constants
-define('ACF_GF_FIELD_VERSION', '1.2.4');
+define('ACF_GF_FIELD_VERSION', '1.2.5');
 define('ACF_GF_FIELD_TEXTDOMAIN', 'acf-gravityforms-add-on');
 define('ACF_GF_FIELD_LANGUAGES', dirname(plugin_basename(__FILE__)) . '/languages/');
 
